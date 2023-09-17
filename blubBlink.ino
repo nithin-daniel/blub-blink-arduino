a8d865c14f2cf92c07668151acf40589a1e8830a
Nithin Daniel <66398646+nithindaniel1@users.noreply.github.com>
2023-09-17 17:29:39 +0530

    Create blubBlink.ino

---
+++ blubBlink.ino
@@ -0,0 +1,13 @@
+void setup() {
+  pinMode (5,OUTPUT);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(5,HIGH);
+  delay(2000);
+  digitalWrite(5,LOW);
+  delay(2000);
+
+}
